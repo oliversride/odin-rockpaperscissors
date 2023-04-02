b57cd05829a9963a46d7c390f3ce78517218e181
Richard Hoffman <oliversride@gmail.com>
2023-04-02 13:27:40 -0700

    Redo after git mistake and update UI

--- script.js
+++ script.js
@@ -16,3 +16,15 @@ function getComputerChoice() {
 for (let i = 0; i < 10; i++) {
   console.log(getComputerChoice());
 }
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('button');
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+    alert(button.id);
+  });
+});
